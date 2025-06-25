c388d8bb0799ac3d7d682ff4e1f0d297dfff0e92
joshuayeyo <chiikawayeyo@gmail.com>
2025-06-25 21:44:12 +0900

    Style(#8): Add 디자인 시스템 Colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -45,6 +45,35 @@ const config: Config = {
         { lineHeight: "1.5", letterSpacing: "-0.01em", fontWeight: "400" },
       ],
     },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#6356f8",
+          50: "#edf0ff",
+          100: "#dee3ff",
+          200: "c4caff",
+          300: "a1a7ff",
+          400: "7c7bfe",
+          500: "#6356f8",
+          600: "5a3eed",
+          700: "4d31d1",
+          800: "#3f2aa9",
+        },
+        caution: "#e4595e",
+        grey: {
+          DEFAULT: "#6D6D6D",
+          50: "#FFFFFF",
+          100: "#E7E7E7",
+          200: "#D1D1D1",
+          300: "#B0B0B0",
+          400: "#888888",
+          500: "#6D6D6D",
+          600: "#4F4F4F",
+          700: "#3D3D3D",
+          800: "111111",
+        },
+      },
+    },
   },
 };
 
