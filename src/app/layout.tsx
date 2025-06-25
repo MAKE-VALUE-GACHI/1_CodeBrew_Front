e8f18bd4c98e69383a45b6bd62d6aa3df77310f8
可口可乐 <chiikawayeyo@gmail.com>
2025-06-25 22:43:53 +0900

    Style(#8): Add 디자인 시스템 폰트 && 컬러 (#9)
    * Style(#8): Add 디자인 시스템 Font
    
    * Style(#8): Add 디자인 시스템 Colors
    
    * Typo(#8): Add 빼먹은 # 기호들 to hex 컬러 코드

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,18 @@
 import { ReactNode } from "react";
+import localFont from "next/font/local";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const pretendard = localFont({
+    src: "/fonts/PretendardVariable.woff2",
+    display: "swap",
+    preload: true,
+    weight: "100 900",
+  });
   return (
-    <html lang='ko'>
+    <html
+      lang='ko'
+      className={pretendard.className}
+    >
       <body>{children}</body>
     </html>
   );
