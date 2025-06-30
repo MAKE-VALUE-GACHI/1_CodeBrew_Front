28383fcb26d577bd9f9f545938a10dcb3b301233
타래 <85001878+meowTarae@users.noreply.github.com>
2025-06-30 11:55:58 +0900

    Feat(#7): 공통 컴포넌트 Card 구현

---
+++ src/components/common/Card/index.tsx
@@ -0,0 +1,37 @@
+import { FontSizeType } from "@/constants/design";
+
+interface CardProps {
+  title: string;
+  description: string;
+  font?: FontSizeType;
+  width?: string;
+  height?: string;
+  onClick?: () => void;
+  className?: string;
+}
+
+export default function Card({
+  title,
+  description,
+  font = "caption",
+  width = "w-100",
+  height = "h-24",
+  onClick,
+  className = "",
+}: CardProps) {
+  const baseClasses = `flex flex-col  justify-center 
+  rounded-lg bg-primary-50 p-6
+  focus:outline-none disabled:cursor-not-allowed truncate ${width} ${height} text-${font}`;
+
+  const finalClasses = `${baseClasses} ${className}`;
+
+  return (
+    <section
+      className={finalClasses}
+      onClick={onClick}
+    >
+      <strong className='text-primary-800'>{title}</strong>
+      <p className='text-grey-600'>{description}</p>
+    </section>
+  );
+}
