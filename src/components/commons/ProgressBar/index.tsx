232bb531f11e1f6f284d8546a42859569c08e6ff
joshuayeyo <chiikawayeyo@gmail.com>
2025-07-02 18:17:15 +0900

    Style(#7): Modify 커스터마이징을 위해 Progress Bar의 barHeight를 Props로 받게 변경

--- src/components/commons/ProgressBar/index.tsx
+++ src/components/commons/ProgressBar/index.tsx
@@ -1,9 +1,14 @@
 import { ProgressBarProps } from "@/types/components/commons/ProgressBar";
 
-const CommonProgressBar = ({ progress }: ProgressBarProps) => {
+const CommonProgressBar = ({
+  barHeight = "h-2.5",
+  progress,
+}: ProgressBarProps) => {
   return (
     <>
-      <div className='h-2.5 w-full overflow-hidden rounded-full bg-background'>
+      <div
+        className={`h-${barHeight} w-full overflow-hidden rounded-full bg-background`}
+      >
         <div
           className='h-full rounded-full bg-primary-300 transition-all duration-300 ease-in-out'
           style={{ width: `${progress}%` }}
