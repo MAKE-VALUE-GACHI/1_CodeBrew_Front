56981188a54ab30b97a05c745a8c57eecf2f9822
타래 <85001878+meowTarae@users.noreply.github.com>
2025-07-02 21:00:05 +0900

    Docs(#7): Input 컴포넌트 Docs 추가

--- src/components/commons/Input/index.tsx
+++ src/components/commons/Input/index.tsx
@@ -5,6 +5,17 @@ import InputDeleteIcon from "./inputDeleteIcon";
 import InputVisibleIcon from "./inputVisibleIcon";
 import { useState } from "react";
 
+/**
+ * Input Component
+ * @param type - 인풋의 타입을 설정합니다. type: default, error
+ * @param text, setText - 유저로부터 입력받은 텍스트를 관리합니다. useState의 형태로 관리합니다.
+ * @param iconType - 인풋의 아이콘 타입을 설정합니다. iconType: text, password
+ * - text: 오른쪽에 삭제 아이콘이 표시됩니다.
+ * - password: 오른쪽에 비밀번호 보기, 가리기 아이콘이 표시됩니다.
+ * @param iconSize - 인풋의 아이콘 크기를 설정합니다. 기본은 18px 입니다.
+ * @param className - 인풋의 컨테이너에 적용되는 커스텀 클래스를 설정합니다.
+ */
+
 export default function Input({
   type,
   id,
