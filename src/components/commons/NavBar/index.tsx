077ea82ee29c323be37336bdaa97a5b2ad3adb33
joshuayeyo <chiikawayeyo@gmail.com>
2025-07-05 09:45:47 +0900

    Style(#7): Modify General Navigation Bar 내부 버튼 공통 컴포넌트로 변경

--- src/components/commons/NavBar/index.tsx
+++ src/components/commons/NavBar/index.tsx
@@ -1,5 +1,6 @@
 import { NavBarProps } from "@/types/components/commons/NavBar";
 import Link from "next/link";
+import Button from "../Button";
 
 const CommonNavBar = ({ user }: NavBarProps) => {
   return (
@@ -13,38 +14,56 @@ const CommonNavBar = ({ user }: NavBarProps) => {
         <div className='flex flex-row gap-8'>
           {/* TODO: 각 서비스별 실제 링크 확정 후 href 수정 필요 */}
           <Link href='/'>
-            <button className='text-body-4 text-grey-700 transition-colors hover:text-grey-600'>
-              서비스 소개
-            </button>
+            <Button
+              type='basicSmall'
+              text='서비스 소개'
+              width={80}
+              height={40}
+              font='body-4'
+            />
           </Link>
           <Link href='/'>
-            <button className='text-body-4 text-grey-700 transition-colors hover:text-grey-600'>
-              하루 메일
-            </button>
+            <Button
+              type='basicSmall'
+              text='하루 메일'
+              width={80}
+              height={40}
+              font='body-4'
+            />
           </Link>
           <Link href='/'>
-            <button className='text-body-4 text-grey-700 transition-colors hover:text-grey-600'>
-              AI 챗봇
-            </button>
+            <Button
+              type='basicSmall'
+              text='AI 챗봇'
+              width={80}
+              height={40}
+              font='body-4'
+            />
           </Link>
         </div>
-        {/* TODO: 버튼 공통 컴포넌트 개발 후 수정 필요 */}
         <div>
           {user ? (
-            <button className='rounded-full border-2 border-solid border-background px-2 py-2 text-caption transition-colors hover:bg-background'>
-              {
-                <img
-                  src={user.imageUrl || "./icons/profile.png"}
-                  alt={user.name || "User"}
-                  className='h-5 w-5'
-                />
-              }
-            </button>
+            // 공통 버튼에 아이콘 버튼이 별도로 없으므로 이대로 유지
+            <Link href='/profile'>
+              <button className='rounded-full border-2 border-solid border-background px-2 py-2 text-caption transition-colors hover:bg-background'>
+                {
+                  <img
+                    src={user.imageUrl || "./icons/profile.png"}
+                    alt={user.name || "User"}
+                    className='h-5 w-5'
+                  />
+                }
+              </button>
+            </Link>
           ) : (
             <Link href='/sign-in'>
-              <button className='rounded-md border-2 border-solid border-background px-2 py-2 text-body-5 transition-colors duration-200 hover:bg-background'>
-                로그인/회원가입
-              </button>
+              <Button
+                type='gnbLogin'
+                text='로그인/회원가입'
+                width={120}
+                height={40}
+                font='body-5'
+              />
             </Link>
           )}
         </div>
