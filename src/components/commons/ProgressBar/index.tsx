6bd82b626096db1df5938fb6a224cc328edcf173
joshuayeyo <chiikawayeyo@gmail.com>
2025-07-05 21:03:56 +0900

    Typo(#7): Modify Progress bar 정상적으로 출력되지 않는 문제 해결

--- src/components/commons/ProgressBar/index.tsx
+++ src/components/commons/ProgressBar/index.tsx
@@ -1,17 +1,15 @@
 import { ProgressBarProps } from "@/types/components/commons/ProgressBar";
 
 const CommonProgressBar = ({
-  barHeight = "h-2.5",
+  barHeight = "10px",
   progress,
 }: ProgressBarProps) => {
   return (
     <>
-      <div
-        className={`h-${barHeight} w-full overflow-hidden rounded-full bg-background`}
-      >
+      <div className={`w-full overflow-hidden rounded-full bg-background`}>
         <div
           className='h-full rounded-full bg-primary-300 transition-all duration-300 ease-in-out'
-          style={{ width: `${progress}%` }}
+          style={{ width: `${progress}%`, height: barHeight }}
         />
       </div>
     </>
