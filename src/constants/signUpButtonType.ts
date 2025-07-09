49df01c574954ea61fa962ee9e5bbbf426dd96ce
타래 <85001878+meowTarae@users.noreply.github.com>
2025-07-09 22:30:40 +0900

    Feat(#17): 로그인 상수값들 추가

---
+++ src/constants/signUpButtonType.ts
@@ -0,0 +1,9 @@
+import { ButtonType } from "@/types/components/feature/SignInPage/SignUpButton";
+
+export const SIGNUP_BUTTON_TYPES: ButtonType[] = [
+  "kakao",
+  "naver",
+  "facebook",
+  "google",
+  "phone",
+];
